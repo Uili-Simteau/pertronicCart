4ce13efd7133a1668f77ac426719a56d8cd624df
Uili-Simteau <simteau@gmail.com>
2016-09-13 12:00:34 +1200

    got github files working for tabs and writing of dispatcher code

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './components/app'
 
-render(<App name='Pertronic Panel Wizard' />, document.querySelector('main'))
+render(
+    <App name='Pertronic Panel Wizard' />, document.querySelector('main')
+    )
 console.log('welcome to Pertronic Panel Wizard')
