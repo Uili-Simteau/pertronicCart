468a9e46f43ac7144c25c34e97aa9f2da9ace7bf
Uili-Simteau <simteau@gmail.com>
2016-09-14 16:37:45 +1200

    finished adding cart logic into cartSelection.js

--- modules/cartSelection.jsx.js
+++ modules/cartSelection.jsx.js
@@ -204,5 +204,139 @@ const CartSelection = React.createClass({
     propTypes: {
         items               : React.PropTypes.object,
         selection           : React.PropTypes.array,
+        onItemAdded         : React.PropTypes.func,
+        onItemRemoved       : React.PropTypes.func,
+        onItemChanged       : React.PropTypes.func,
+        onChange            : React.PropTypes.func,
+        iterator            : React.PropTypes.func,
+        tableClassName      : React.PropTypes.string,
+        cartEmptyMessasge   : React.PropTypes.node,
+    },
+    getDefaultProps() {
+        return {
+            items               : {},
+            selection           : [],
+            onItemAdded         : () => {},
+            onItemRemoved       : () => {},
+            onItemQtyChanged    : () => {},
+            onChange            : () => {},
+            iterator            : () => { return {} },
+            containerComponent  : ContainerComponent,
+            rowComponent        : RowComponent,
+            tableClassName      : '',
+            cartEmptyMessage    : (
+                <span>
+                    The cart is empty.
+                </span>
+            )
+        }
+    },
+    getInitialState() {
+        return {
+            selection : []
+        }
+    },
+    refresh() {
+        this.setState({
+            selection : CartStore.getSelection()
+        })
+    },
+    onChange() {
+        this.refresh()
+        this.props.onChange()
+    },
+    componentDidMount() {
+        CartStore.on('ready', this.refresh)
+        CartDispatcher.dispatch({
+            actionType : 'cart-initialize',
+            config     : {
+                items        : this.props.items,
+                selection    : this.props.selection
+            }
+        })
+        CartStore.on('change', this.onChange)
+        CartStore.on('item-added', this.props.onItemAdded)
+        CartStore.on('item-removed', this.props.onItemRemoved)
+        CartStore.on('item-changed', this.props.onItemQtyChanged)
+    },
+    componentWillUnmount() {
+        CartStore.removeListener('ready', this.refresh)
+        CartStore.removeListener('change', this.onChange)
+        CartStore.removeListener('item-added', this.props.onItemAdded)
+        CartStore.removeListener('item-removed', this.props.onItemRemoved)
+        CartStore.removeListener('item-changed', this.props.onItemQtyChanged)
+    },  
+    addItem(key, quantity, item) {
+        CartDispatcher.dispatch({
+            actionType : 'cart-add-item',
+            key        : key,
+            quantity   : quantity,
+            item       : item
+        })
+    },
+    removeItem(index) {
+        CartDispatcher.dispatch({
+            actionType : 'cart-remove-item',
+            index      : index
+        })
+    },
+    updateQuantity(index, quantity) {
+        CartDispatcher.dispatch({
+            actionType : 'cart-update-item',
+            index      : index,
+            quantity   : quantity
+        })
+    },
+    emptyCart() {
+        CartDispatcher.dispatch({
+            actionType : 'cart-reset'
+        })
+    },
+    reset() {
+        CartDispatcher.dispatch({
+            actionType : 'cart-revert',
+            config     : {
+                items        : this.props.items,
+                selection    : this.props.selection
+            }
+        })
+    },
+    isEmpty() {
+        return CartStore.isEmpty()
+    },
+    getSelection() {
+        return CartStore.getSelection()
+    },
+    render() {
+        let context   = this.props.iterator(),
+            Container = this.props.containerComponent,
+            Row       = this.props.rowComponent
+        if (this.isEmpty()) {
+            return (
+                <div>
+                    {this.props.cartEmptyMessage}
+                </div>
+            )
+        }
+        return (
+            <Container
+              tableClassName = {this.props.tableClassName}
+              columns        = {this.props.columns}
+              body           = {this.state.selection.map(item => {
+                  context = this.props.iterator(context, item)
+                  return (
+                      <Row
+                        key        = {item._key}
+                        item       = {item}
+                        columns    = {this.props.columns}
+                        removeItem = {()  => this.removeItem(item._index)}
+                        setItemQty = {qty => this.updateQuantity(item._index, qty)} />
+                  )
+              })}
+              context        = {context} 
+            />
+        )
     }
 })
+
+module.exports = CartSelection
