a73d78b5b856801e1f360ad55ced0f61f101329e
Uili-Simteau <simteau@gmail.com>
2016-09-14 16:23:03 +1200

    adding cart render components

--- modules/cartSelection.jsx.js
+++ modules/cartSelection.jsx.js
@@ -163,3 +163,46 @@ const ContainerComponent = React.createClass({
     }
 })
 
+const RowComponent =React.createClass({
+    handleChange(event) {
+        const value = event.target.value
+        if (!isNaN(value) && value > 0) {
+                this.props.setItemQty(value)
+        }
+    },
+
+    render() {
+        return (
+            <tr>
+                {this.props.coloumns.map(column => {
+                    return (
+                        <td key={column}>
+                            {this.props.item.data[column]}
+                        </td>
+                    )
+                })}
+                <td>
+                    <input 
+                        style    = {{textAlign: 'right', width: '100px'}}
+                        type     = 'number'
+                        value    = {this.props.item.quantity}
+                        onChange = {this.handleChange}
+                        />
+                </td>
+                <td>
+                    <button
+                        onClick  = {this.props.removeItem}>
+                        Remove
+                    </button>
+                </td>
+            </tr>
+        )
+    }
+})
+
+const CartSelection = React.createClass({
+    propTypes: {
+        items               : React.PropTypes.object,
+        selection           : React.PropTypes.array,
+    }
+})
