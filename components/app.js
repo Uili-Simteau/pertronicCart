e28d0802a0a4e52c8f96a2dbd39f561aa90055f4
Uili-Simteau <simteau@gmail.com>
2016-09-27 22:09:05 +1300

    setting up tab contents in line with pricelist

--- components/app.js
+++ components/app.js
@@ -33,13 +33,86 @@ class App extends Component {
             </form>
           </Tab>
           <Tab eventKey={2} title="System">
-            <h2>System</h2>
+            <h2>Panel Selection</h2>
             <form>
                 <Radio name="system" data-toggle="Button" >
-                  Conventional                    
+                  <span>F120A   </span>       
+                  <select name="cabinet_size" size="1">
+                      <option value="F120AF-3">
+                          FS 2 Loop Panel 900mm
+                      </option>
+                      <option value="F120AR-3">
+                          RS 2 Loop Panel 900mm
+                      </option>
+                  </select>                
                 </Radio>
                 <Radio name="system">
-                    Analog
+                    <span>F100A   </span>
+                    <select name="cabinet_size" size="1">
+                        <option value="F100AF-3">
+                            FS 2 Loop Panel 600mm
+                        </option>
+                        <option value="F100AR-3">
+                            RS 2 Loop Panel 600mm
+                        </option>
+                        <option value="F100ATF-3">
+                            FS 2 Loop Panel 900mm
+                        </option>
+                        <option value="F100ATR-3">
+                            RS 2 Loop Panel 900mm
+                        </option>
+                    </select>
+                </Radio>
+                <Radio name="system">
+                    <span>F16E   </span>
+                    <select name="cabinet_size" size="1">
+                        <option value="F16EMF">
+                            F16e 8cct Mini FS
+                        </option>
+                        <option value="F16EMR">
+                            F16e 8cct Mini RS
+                        </option>
+                        <option value="F16ELF">
+                            F16e 8cct Large FS
+                        </option>
+                        <option value="F16ELR">
+                            F16e 8cct Large RS
+                        </option>
+                        <option value="F16ETF">
+                            F16e 8cct Tall FS
+                        </option>
+                        <option value="F16ETR">
+                            F16e 8cct Tall RS
+                        </option>
+                    </select>
+                </Radio>
+                <Radio name="system">
+                    <span>F4   </span>
+                    <select name="F4" size="1">
+                        <option value="F4FS-3">
+                            F4FS
+                        </option>
+                        <option value="F4RS-3">
+                            F4RS
+                        </option>
+                        <option value="F4FS-3EA">
+                            F4FS + EVAC20W12V
+                        </option>
+                        <option value="F4RS-3EA">
+                            F4RS + EVAC20W12V
+                        </option>
+                    </select>
+                </Radio>
+                <Radio name="system">
+                    <span>F1   </span>
+                    <select name="F1" size="1">
+                        <option value="F1-3">
+                            F1
+                        </option>
+                        <option value="F1-3EA">
+                            F1 + EVAC20W12V
+                        </option>
+                    </select>
                 </Radio>
             </form>
           </Tab>
